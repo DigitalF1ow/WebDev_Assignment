28217d2683ced03c05fb05151771889b7783318d
DigitalF1ow <darrenabbot00@gmail.com>
2021-02-16 23:22:45 +0800

    Header and Footer Admin Updated

---
+++ admin/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home - Administrator Panel</title>
+    <link rel="stylesheet" href="../css/admin.css">
+</head>
+<body>
+    <?php include 'partials/header.php' ?>
+
+    <!--Make it to become a grid size while expand or smaller the screen-->
+    <?php include 'partials/footer.php' ?>
+</body>
+
+
+</html>
