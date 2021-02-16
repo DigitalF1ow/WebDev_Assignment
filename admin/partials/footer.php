b9e0f51867564bf6936916dd2da5ce5e1240264c
soulRix <1181101025@student.mmu.edu.my>
2021-02-16 09:23:01 +0800

    Update

---
+++ admin/partials/footer.php
@@ -0,0 +1,10 @@
+<!-- Footer Section Start -->
+<div class =" footer">
+            <div class = "wrapper">
+                <p class = "text-center">2020 All Right Reserved, This Web Dev make me nuts <a href = "#">Chicks</a></p>
+            </div>
+        </div>
+        <!-- Footer Section Ends -->
+    <body>
+
+</html>
---
+++ admin/partials/menu.php
@@ -0,0 +1,23 @@
+<html>
+    <head>
+        <title>Booking Tour Guide - Home Page</title>
+        
+        <link rel="stylesheet" href="../css/admin.css">
+    </head>
+
+
+    <body>
+        <!-- Booking Section Start -->
+        <div class ="menu text-center">
+            <div class = "wrapper">
+                <ul>
+                    <li><a href = "index.php">Home</a></li>
+                    <li><a href = "manage-admin.php">Admin</a></li>
+                    <li><a href = "manage-category.php">Category</a></li>
+                    <li><a href = "#">Destination</a></li>
+                    <li><a href = "#">Book Tour</a></li>
+                </ul>
+            </div>
+        </div>
+        <!-- Booking Section Ends -->
+        
