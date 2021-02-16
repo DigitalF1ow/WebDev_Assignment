42e5f83d067624afc77108fd5ad2db3688c624ed
Levotier <puakhefong@yahoo.com>
2021-02-16 21:59:46 +0800

    Footer Header

---
+++ php/responsiveFooter.php
@@ -0,0 +1,9 @@
+<?php
+
+    echo "<footer class='footer'>";
+    echo "<a href='index.html'><img src = 'images/visitKL.png' class='footer-img' alt='homeIcon'></a>";
+    echo "<p class='footer-text'>Copyright &copy; Visit Kuala Lumpur 2021</p>";
+    echo "<p class='footer-text'>Made by courtesy of The Group of Life - Web Development Assignment</p>";
+    echo "</footer>";
+
+?>
