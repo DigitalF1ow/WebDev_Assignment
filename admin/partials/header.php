cebcb9746ae3f9362393fc5ace517c8a4de32752
DigitalF1ow <darrenabbot00@gmail.com>
2021-02-17 11:09:25 +0800

    Remove Unneeded PHP

--- admin/partials/header.php
+++ admin/partials/header.php
@@ -2,7 +2,6 @@
     <nav>
         <ul>
             <li><a href="index.php">Home</a></li>
-            <li><a href="manage-admins.php">Manage Admins</a></li>
             <li><a href="manage-activities.php">Manage Activities</a></li>
             <li><a href="manage-destinations.php">Manage Destinations</a></li>
             <li><a href="manage-reservations.php">Manage Reservations</a></li>
