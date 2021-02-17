77e0ec8ef1b61a3cd3b444297d2a7624ae740c9f
DigitalF1ow <darrenabbot00@gmail.com>
2021-02-17 11:05:30 +0800

    Added Universal Formatting for Admin Panel
    Admin Panels are now universal, edit as you please.

---
+++ admin/add-destination.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name = "viewport" content = "width=device-width, initial-scale = 1.0">
+    <title>Home - Administrator Panel</title>
+    <link rel="stylesheet" href="../css/admin.css">
+
+    <link href="https://fonts.googleapis.com/css?family=Open+Sans&display=swap" rel="stylesheet">
+</head>
+<body>
+    <?php include 'partials/header.php' ?>
+    <!--Make it to become a grid size while expand or smaller the screen-->
+    <section class="main-content ">
+        <h1>Add Destination</h1>
+    </section>
+    
+
+    <!--End of the main content-->
+    <?php include 'partials/footer.php' ?>
+</body>
+
+</html>
