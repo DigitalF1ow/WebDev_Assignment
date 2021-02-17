b7d4df80131f7d8986ea2eb6e1a8a2452f43b7e9
Levotier <puakhefong@yahoo.com>
2021-02-17 13:16:25 +0800

    Update add-activity.php

--- admin/add-activity.php
+++ admin/add-activity.php
@@ -14,10 +14,55 @@
         <h1>Add Activity</h1>
     </section>
     
+    <!--<div class= "main-content">
+    <div class= "wrapper">
+            <h1> Add Activity</h1> -->
+
+            <br><br>
+
+            <!-- Add Category Form Starts -->
+            <form action= "" method= "POST">
+
+                <table class ="tbl-30">
+                    <tr>
+                        <td>Name: </td>
+                        <td>
+                            <input type = "text" name= "activity_name" placeholder = "Activity name">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Description: </td>
+                        <td>
+                            <textarea name= "activity_desc" cols = "30" rows = "10" placeholder= "Descripton of the Activity"></textarea>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Select Image: </td>
+                        <td>
+                            <input type = "file" name= "activity_image">
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td>Image Name: </td>
+                        <td>
+                            <input type = "text" name= "activity_alt" placeholder = "Alternate name for Image">
+                        </td>
+                    </tr>
+
+
+                </table>
+            </form>
+            <!-- Add Category Form Ends -->
+    </div>
 
     <!--End of the main content-->
     <?php include 'partials/footer.php' ?>
 </body>
 
 
-</html>
+
+
+
